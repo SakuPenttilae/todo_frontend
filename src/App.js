c3e9c3d4d14dc8408d98210d7f5feb04d55c4632
Saku Penttilä <sakuinenpenttila@gmail.com>
2022-03-12 16:22:04 +0200

    haku ja lisäys toimii frontendissä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const URL = "http://localhost/todo/"
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState("")
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        //console.log(response.data)
+        setTasks(response.data)
+      }).catch(error => {
+        //yhden rivin if else lause
+        alert(error.response ? error.response.data.error : error)
+        /*if (error.response) {
+          alert(error.response.data.error)
+        } else {
+          alert(error)
+        }*/
+      }) 
+  }, [])
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:task})
+    axios.post(URL+"add.php", json, {
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    }) .then((response) => {
+      setTasks(tasks => [...tasks, response.data])
+      setTask("")
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={save}>
+        <label>New Task</label>
+        <input value={task} placeholder='laita uutta täskiä' onChange={e => setTask(e.target.value)}/>
+        <button>save</button>
+      </form>
+      <ol>
+        {tasks?.map(task => (
+          <li key={task.id}>{task.description}</li>
+        ))}
+      </ol>
     </div>
   );
 }
